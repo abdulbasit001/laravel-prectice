d90b27e2c5778a19f2fb52af5873f6fe9ec530c8
abdulbasit001 <abdul.basit@shottechnologies.co>
2022-01-28 03:11:08 +0500

    updates

---
+++ resources/views/pizzas.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <title>Laravel</title>
+   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+
+<body class="antialiased">
+   <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+
+
+      <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+         <h1>
+            Pizzas
+         </h1>
+         <p>
+            {{$user_first_name }} - {{$user_last_name}} - {{$user_name}} - {{$user_email}}
+         </p>
+      </div>
+   </div>
+</body>
+</html>
